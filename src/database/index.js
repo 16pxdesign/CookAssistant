5b09ff12d70a24f0a96a618f64e98971c6e66da0
Aruzo <dawid12391111@gmail.com>
2022-06-04 13:21:19 +0200

    configured docker

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2/promise";
+import waitPort from "wait-port";
 
 const {
   MYSQL_HOST: HOST,
@@ -7,11 +8,54 @@ const {
   MYSQL_DB: DB,
 } = process.env;
 
+await waitPort({ host: HOST, port: 3306 });
+
 const db = await mysql.createConnection({
   host: HOST,
   user: USER,
   password: PASSWORD,
   database: DB,
 });
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS categories (
+      id int AUTO_INCREMENT,
+      name varchar(255) NOT NULL,
+      PRIMARY KEY (id)
+  )`);
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS categories_parents (
+      category_id int NOT NULL,
+      parent_id int DEFAULT 0,
+      PRIMARY KEY (category_id, parent_id),
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+  )`);
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS recipes (
+      id int AUTO_INCREMENT,
+      name varchar(255) NOT NULL,
+      PRIMARY KEY (id)
+  )`);
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS categories_recipes (
+      category_id int NOT NULL,
+      recipe_id int NOT NULL,
+      PRIMARY KEY (category_id, recipe_id),
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+      FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+  )`);
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS products (
+      id int AUTO_INCREMENT,
+      recipe_id int NOT NULL,
+      name varchar(255) NOT NULL,
+      amount int NOT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+  )`);
 
 export default db;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import routes from "./routes/index.js";
 
-const PORT = process.env.PORT;
-
 const app = express();
 
 app.set("view engine", "ejs");
+app.set("views", "src/views");
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => console.log("Server started at port: " + PORT));
+app.listen(3000, () => console.log("Server started at port: 3000"));
