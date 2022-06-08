e982472ac4a03f9df94714414645877b7e308a54
Aruzo <dawid12391111@gmail.com>
2022-06-08 19:18:24 +0200

    updated initializing of database

--- src/database/index.js
+++ src/database/index.js
@@ -17,45 +17,4 @@ const db = await mysql.createConnection({
   database: DB,
 });
 
-await db.query(`
-  CREATE TABLE IF NOT EXISTS categories (
-      id int AUTO_INCREMENT,
-      name varchar(255) NOT NULL,
-      PRIMARY KEY (id)
-  )`);
-
-await db.query(`
-  CREATE TABLE IF NOT EXISTS categories_parents (
-      category_id int NOT NULL,
-      parent_id int DEFAULT 0,
-      PRIMARY KEY (category_id, parent_id),
-      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
-  )`);
-
-await db.query(`
-  CREATE TABLE IF NOT EXISTS recipes (
-      id int AUTO_INCREMENT,
-      name varchar(255) NOT NULL,
-      PRIMARY KEY (id)
-  )`);
-
-await db.query(`
-  CREATE TABLE IF NOT EXISTS categories_recipes (
-      category_id int NOT NULL,
-      recipe_id int NOT NULL,
-      PRIMARY KEY (category_id, recipe_id),
-      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
-      FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
-  )`);
-
-await db.query(`
-  CREATE TABLE IF NOT EXISTS products (
-      id int AUTO_INCREMENT,
-      recipe_id int NOT NULL,
-      name varchar(255) NOT NULL,
-      amount int NOT NULL,
-      PRIMARY KEY (id),
-      FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
-  )`);
-
 export default db;
